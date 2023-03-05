8e1b74c9eaf9a0e066abd6214b76a03fb6b554bd
vitor <vitor.grosskopf@gmail.com>
2023-03-05 15:34:24 -0300

    Changing routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:show]
   root 'pages#home'
   get 'about', to: 'pages#about'
+  resources :articles, only: [:show, :index, :new, :creat]
 end
