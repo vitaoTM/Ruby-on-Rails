dbdc698e059f3abbc8dd04ead647bded0684a11a
vitor <vitor.grosskopf@gmail.com>
2023-03-08 14:08:05 -0300

    Add create and update methods; * add routes for create and update path * add edit html page on views>articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(params.require(:article).permit(:title, :description))
     if @article.save
@@ -20,5 +24,18 @@ class ArticlesController < ApplicationController
       # redirect_to article_path(@article) # Long version redirect
     else
       render 'new'
+    end
   end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(params.require(:article).permit(:title, :description))
+      flash[:notice] = "Article was updated successfully"
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+
 end
