1426bb600962b5b0424b2480499c7ec92a911690
vitor <vitor.grosskopf@gmail.com>
2023-03-10 10:18:55 -0300

    creating new articles table from scratch with new migration

--- db/migrate/20230310131442_create_articles.rb
+++ db/migrate/20230310131442_create_articles.rb
@@ -3,6 +3,8 @@ class CreateArticles < ActiveRecord::Migration[6.0]
     create_table :articles do |t|
       t.string :title
       t.text :description
+      add_column :articles, :created_at, :datetime
+      add_column :articles, :updated_at, :datetime
     end
   end
 end
